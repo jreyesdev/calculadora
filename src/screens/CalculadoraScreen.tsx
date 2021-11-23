f20b79e67895c4b396eacdc0bdb8e752087ac457
jreyesdev <djrm1204@gmail.com>
2021-11-23 14:33:26 -0400

    seccion resultado y operacion

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export const CalculadoraScreen = () => {
   return (
-    <View>
-      <Text>Calculadora</Text>
+    <View style={styles.container}>
+      <Text style={styles.operacion}>1,500.00</Text>
+      <Text style={styles.resultado}>1,500.00</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+  },
+  resultado: {
+    color: 'white',
+    fontSize: 60,
+    textAlign: 'right',
+  },
+  operacion: {
+    color: 'rgba(255,255,255,0.5)',
+    fontSize: 30,
+    textAlign: 'right',
+  },
+});
