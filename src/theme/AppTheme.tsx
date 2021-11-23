6ef14c9d65d05f1be69f4d93e3a07a911597ff72
jreyesdev <djrm1204@gmail.com>
2021-11-23 14:24:58 -0400

    tema

---
+++ src/theme/AppTheme.tsx
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  tema: {
+    flex: 1,
+    backgroundColor: 'black',
+    color: 'white',
+    fontSize: 20,
+  },
+});
